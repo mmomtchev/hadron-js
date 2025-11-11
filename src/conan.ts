786650b9c8a99ee303bd3aeac82cf2e8b4c98e46
Momtchil Momtchev <momtchil@momtchev.com>
2025-11-11 14:04:58 +0100

    add more TS types

--- src/conan.ts
+++ src/conan.ts
@@ -5,7 +5,7 @@ import assert from 'node:assert';
 import { ConanOption, Environment, getNpmOption, optionEquivalence, OptionVal, quote } from './util.js';
 
 export function conanBuildOptions(path: string): ConanOption[] {
-  let o, r;
+  let o: Buffer, r: { options_definitions: ConanOption[]; };
 
   try {
     fs.statSync('conanfile.py');
